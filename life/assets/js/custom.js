9cb4fea594005c24f27a0d32d1ddd85bf10727b9
dmitry <dbukatyy@mail.ru>
2018-11-02 22:00:13 +0300

    finish

--- life/assets/js/custom.js
+++ life/assets/js/custom.js
@@ -3,6 +3,10 @@
 jQuery(document).ready(function () {
 
   var bottomNav = document.querySelector('.bottom-nav');
+  var usefulls = document.querySelector('.usefulls');
+
+  $('.images__left').addClass('bounceInLeft animated');
+  $('.images__right').addClass('bounceInRight animated');
 
   $('.reviews__slider').slick({
     vertical: true,
@@ -14,10 +18,24 @@ jQuery(document).ready(function () {
     nextArrow: $('.slider-next')
   });
 
+  $('.price').on('mouseover', function (e) {
+    $('.price').removeClass('active');
+    $(e.currentTarget).addClass('active');
+  });
+
+  $('.menu__control').on('click', function (e) {
+    $(e.currentTarget).toggleClass('active');
+    $(e.currentTarget).siblings('.menu').toggleClass('active');
+  });
+
   $(document).on('scroll', function (e) {
     window.scrollY > 260 ? $('.header').addClass('active') : $('.header').removeClass('active');
 
-    if (bottomNav.getBoundingClientRect().top < window.innerHeight) {
+    if (usefulls.getBoundingClientRect().top < window.innerHeight / 2) {
+      $('.usefull').addClass('animated flipInX');
+    }
+
+    if (bottomNav.getBoundingClientRect().top < window.innerHeight && window.innerWidth > 992) {
       $('.header').addClass('hide');
     } else {
       $('.header').removeClass('hide');
@@ -39,12 +57,19 @@ jQuery(document).ready(function () {
     var section = $(e.currentTarget).data('section');
 
     $('body, html').animate({
-      scrollTop: $('' + section).offset().top - 60
+      scrollTop: $('' + section).offset().top - 55
     }, 1000);
   });
 
   function initScroll() {
     $('.scroll').baron({
+      scroller: '.baron__scroller',
+      bar: '.baron__bar',
+      scrollingCls: '_scrolling',
+      draggingCls: '_dragging'
+    });
+
+    $('.scroll-v').baron({
       scroller: '.baron__scroller',
       bar: '.baron__bar',
       scrollingCls: '_scrolling',
@@ -97,7 +122,14 @@ jQuery(document).ready(function () {
     var $this = $(e.currentTarget),
         window = $this.data('wind'),
         data = $('' + window).html(),
-        popUp = $('.pop-up__wrapper');
+        popUp = $('.pop-up__wrapper'),
+        colored = $('' + window).hasClass('colored');
+
+    if (colored) {
+      $('.pop-up').addClass('pop-up_colored');
+    } else {
+      $('.pop-up').removeClass('pop-up_colored');
+    }
 
     popUp.find('.pop-up__inner').html(data);
     initRange();
