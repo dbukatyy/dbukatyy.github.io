d6bd4ee49a7f76c0c4114c56cb9509a8bd04d9cd
dmitry <dbukatyy@mail.ru>
2018-11-20 21:09:09 +0300

    add modal

--- life2/build/assets/js/custom.js
+++ life2/build/assets/js/custom.js
@@ -1,6 +1,30 @@
-"use strict";
+'use strict';
 
 jQuery(document).ready(function () {
+
+  // modal
+  var modal = $('.modal-block');
+
+  modal.on('click', function (e) {
+    var el = $(e.target);
+    var insideModal = el.closest('.modal').hasClass('modal');
+    var isClose = el.hasClass('modal__close') || el.closest('.modal__close').hasClass('modal__close');
+    if (!insideModal || isClose) {
+      modal.fadeOut(300);
+    }
+  });
+
+  $('.js-modal').on('click', function (e) {
+    e.preventDefault();
+    var wind = $(this).data('modal');
+
+    $('#' + wind).fadeIn(300);
+  });
+
+  $('.password__toggle').on('click', function (e) {
+    e.preventDefault();
+    $(e.currentTarget).siblings('input').attr('type', 'text');
+  });
 
   $('.sloder-items').slick({
     dots: true,
--- life2/src/js/custom.js
+++ life2/src/js/custom.js
@@ -1,5 +1,29 @@
 jQuery(document).ready(function () {
 
+  // modal
+  const modal = $('.modal-block');
+
+  modal.on('click', (e) => {
+    const el = $(e.target);
+    const insideModal = el.closest('.modal').hasClass('modal');
+    const isClose = el.hasClass('modal__close') || el.closest('.modal__close').hasClass('modal__close');
+    if (!insideModal || isClose) {
+      modal.fadeOut(300);
+    }
+  })
+
+  $('.js-modal').on('click', function (e) {
+    e.preventDefault();
+    const wind = $(this).data('modal');
+
+    $(`#${wind}`).fadeIn(300);
+  })
+
+  $('.password__toggle').on('click', function (e) {
+    e.preventDefault();
+    $(e.currentTarget).siblings('input').attr('type','text');
+  })
+
   $('.sloder-items').slick({
     dots: true,
     slidesToShow: 2,
